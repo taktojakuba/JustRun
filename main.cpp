c82de3eb7714dd0ade788a70d04788ea284ee248
taktojakuba <112001303+taktojakuba@users.noreply.github.com>
2026-08-15 12:52:38 +0200

    fix stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include <cstdlib>
 #include <unistd.h>
 
 using namespace std;
 
-int main(int argc, cahr* argv[])
+int main(int argc, char* argv[])
 {
   //check argc
   if(argc < 2){
@@ -14,12 +15,10 @@ int main(int argc, cahr* argv[])
   // __NV_PRIME_RENDER_OFFLOAD=1
   // __VK_LAYER_NV_optimus=NVIDIA_only
   // __GLX_VENDOR_LIBRARY_NAME=nvidia
-  setenv("justrun-render", "__NV_PRIME_RENDER_OFFLOAD=1", 1);
-  setenv("justrun-optimus", "__VK_LAYER_NV_optimus=NVIDIA_only
-", 1);
-  setenv("justrun-glx", "__GLX_VENDOR_LIBRARY_NAME=nvidia
-", 1);
+  setenv("__NV_PRIME_RENDER_OFFLOAD", "1", 1);
+  setenv("__VK_LAYER_NV_optimus", "NVIDIA_only", 1);
+  setenv("__GLX_VENDOR_LIBRARY_NAME", "nvidia", 1); 
     // replace justrun with argv2 proc
     execvp(argv[1], &argv[1]);
-    return 0;
+    return 1;
 }
